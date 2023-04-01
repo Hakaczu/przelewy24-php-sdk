82bee3f7406acb033e319dd9d365b4839f596ddf
Sebastian Szypulski <sszypulski.kess@gmail.com>
2023-04-01 21:52:13 +0200

    Add Tests

---
+++ tests/AuthTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Hakaczu\Przelewy24PhpSdk;
+use PHPUnit\Framework\TestCase;
+final class AuthTest extends TestCase {
+
+    public function testClassConstructor(){
+        $auth = new Auth(
+            $_ENV['urlAuth'],
+            $_ENV['username'],
+            $_ENV['password']
+        );
+        
+        $this->assertSame($_ENV['urlAuth'], $auth->url);
+        $this->assertSame($_ENV['username'], $auth->username);
+        $this->assertSame($_ENV['password'], $auth->password);
+    }
+
+    public function testConnectionToApi(){
+        $auth = new Auth(
+            $_ENV['urlAuth'],
+            $_ENV['username'],
+            $_ENV['password']
+        );
+        $auth->connection();
+        $this->assertSame(200, $auth->httpCode);
+    }
+}
