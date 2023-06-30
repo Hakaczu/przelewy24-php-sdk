34c0b07051d0f29a9afcd07c9fbec3509ee27983
Sebastian Szypulski <sszypulski.kess@gmail.com>
2023-06-30 08:14:12 +0200

    Add default value for channel

--- src/Transaction.php
+++ src/Transaction.php
@@ -10,7 +10,7 @@ class Transaction {
     public $email = null;
     public $country = 'PL';
     public $language = 'pl';
-    public $channel = null;
+    public $channel = 16;
 
     public function __construct($merchantId, $posId, $sessionId) {
         $this->merchantId = $merchantId;
